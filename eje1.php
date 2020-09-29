24733d663f52cc05aae52ef624ecf575f316a61a
mecc <mecc15@gmail.com>
2020-09-29 17:30:15 -0600

    nueva version

---
+++ eje1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title></title>
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">	
+	</head>
+	<body>
+		<header>
+			<h1></h1>
+		</heder>
+		
+		<section>
+		</section>
+		
+		<footer>
+		</footer>	
+	</body>	
+</html>	
